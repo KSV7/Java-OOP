201e51db15232058bf32f43fbc2022ed74052cc4
sergey <sergey.kutepov89@gmail.com>
2021-11-07 21:23:48 +0200

    OOP Exception polymorphism

---
+++ Group.java
@@ -0,0 +1,89 @@
+package com.gmail.kutepov89.sergey;
+
+import java.util.Arrays;
+
+public class Group {
+	Student[] students = new Student[10];
+
+	public Group(Student[] students) {
+		super();
+		this.students = students;
+	}
+
+	public Group() {
+		super();
+	}
+
+	public void add(Student student) {
+		for (int i = 0;; i++) {
+			try {
+				if (i >= students.length) {
+					throw new FullGroupException();
+				}
+
+				if (students[i] == null) {
+					students[i] = student;
+					break;
+				}
+			} catch (FullGroupException e) {
+				System.err.println(e.getMessage());
+				break;
+			}
+		}
+	}
+
+	public String del(String lastName) {
+		for (int i = 0; i < students.length; i++) {
+			if (students[i] != null && students[i].getLastName() == lastName) {
+				students[i] = null;
+				return "студент " + lastName + " удален";
+			}
+		}
+		return "Такого студента нет в списке";
+	}
+
+	public Student searchByLastName(String lastName) {
+		for (int i = 0; i < students.length; i++) {
+			if (students[i] != null && students[i].getLastName() == lastName) {
+				return students[i];
+			}
+		}
+		return null;
+	}
+
+	public void sortByLastName(Student[] students) {
+		Student temp;
+		for (int i = 0; i < students.length; i++) {
+			if (students[i] != null) {
+				for (int j = 0; j < students.length - 1; j++) {
+					if (students[j] != null && students[j + 1] != null) {
+						if (students[j].getLastName().compareToIgnoreCase(students[j + 1].getLastName()) > 0) {
+							temp = students[j];
+							students[j] = students[j + 1];
+							students[j + 1] = temp;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+
+		sortByLastName(students);
+
+		stringBuilder.append("\n");
+		for (Student s : students) {
+			if (s != null) {
+				stringBuilder.append(s.getLastName() + " " + s.getFirstName() + " (age: " + s.getAge() + ")")
+						.append("\n");
+			}
+		}
+		stringBuilder.append("\n");
+
+		return stringBuilder.toString();
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,46 @@
+package com.gmail.kutepov89.sergey;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student st = new Student("Vasya", "Vasiliev", "Davidovich", 21, true, 1);
+		Student st2 = new Student("Alex", "Alexgov", "Alexsandrovich", 32, true, 2);
+
+		Student st3 = new Student("Serg", "Williams", "Sergeevich", 23, true, 3);
+		Student st4 = new Student("Ben", "Peters", "Petrovich", 24, true, 4);
+
+		Student st5 = new Student("Tom", "Gibson", "Tomsovich", 19, true, 5);
+		Student st6 = new Student("Andrey", "Martin", "Andreevich", 20, true, 6);
+
+		Student st7 = new Student("Vitaliy", "Jordan", "Vitalievich", 26, true, 7);
+		Student st8 = new Student("John", "Jackson", "Jonsovich", 31, true, 8);
+
+		Student st9 = new Student("Oleg", "Grant", "Olegovich", 30, true, 9);
+		Student st10 = new Student("Igor", "Davis", "Igorevich", 22, true, 10);
+
+		Student st11 = new Student("Vlad", "Lewis", "Vladimirovich", 21, true, 11);
+
+		Group group = new Group();
+
+		group.add(st);
+		group.add(st2);
+		group.add(st3);
+		group.add(st4);
+		group.add(st5);
+		group.add(st6);
+		group.add(st7);
+		group.add(st8);
+		group.add(st9);
+		group.add(st10);
+		group.add(st11);
+
+		System.out.println(group.del("Lewis"));
+
+		System.out.println(group.searchByLastName("Gibson"));
+
+		System.out.println(group.del("Williams"));
+
+		System.out.println(group);
+	}
+
+}
